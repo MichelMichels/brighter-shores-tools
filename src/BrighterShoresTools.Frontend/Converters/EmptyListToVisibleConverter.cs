78c63b2ddc30ab2bafc9556a3a93168823fbe99b
Michel Michels <michel@miche.ls>
2024-11-10 14:17:14 +0100

    ListBox -> DataGrid

---
+++ src/BrighterShoresTools.Frontend/Converters/EmptyListToVisibleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BrighterShoresTools.Frontend.Converters;
+
+public class EmptyListToVisibleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not IEnumerable<object> enumerable)
+        {
+            return Visibility.Collapsed;
+        }
+
+        return !enumerable.Any() ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
