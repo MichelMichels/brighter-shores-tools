1aa3aa33eab1858ec5a85a701998e6911beb26f0
tarantoa <ajdt703@gmail.com>
2024-11-19 19:42:16 -0800

    Add AlchemistTest class library.

---
+++ src/BrighterShoresTools.Professions.AlchemistTest/Services/AlchemistUnlockRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using BrighterShoresTools.Professions.Alchemist.Services;
+using BrighterShoresTools.Professions.Core.Models;
+
+namespace BrighterShoresTools.Professions.AlchemistTests.Services;
+
+[TestClass()]
+public class AlchemistUnlockRepositoryTests
+{
+    [TestMethod()]
+    public async Task GetAllTestAsync()
+    {
+        // Arrange
+        AlchemistUnlockRepository repository = new();
+
+        // Act
+        List<ProfessionUnlock> actions = await repository.GetAll();
+
+        // Assert
+        Assert.IsNotNull(actions);
+    }
+}
